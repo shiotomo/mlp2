4489ed7e25ff435714eaa0dbb8520686e519e0c2
shiotomo <t.shiota16@gmail.com>
2019-02-10 02:25:46 +0900

    install.shを追加した

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+macos() {
+  brew install pyenv
+}
+
+ubuntu() {
+  sudo apt-get update
+  sudo apt-get -y upgrade
+
+  sudo apt-get -y install python-pygame
+  sudo apt-get -y install python-pip
+  sudo apt-get -y install python-tk tk-dev
+
+  sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+  libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+
+  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+}
+
+case "$1" in
+  macos)
+    macos
+    ;;
+  ubuntu)
+    ubuntu
+    ;;
+esac
