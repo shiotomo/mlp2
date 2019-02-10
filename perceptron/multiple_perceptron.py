ab745945d20bd086930ba9e63b209da0de03ffa3
shiotomo <t.shiota16@gmail.com>
2019-02-10 18:51:52 +0900

    XORゲートを作成した

--- perceptron/multiple_perceptron.py
+++ perceptron/multiple_perceptron.py
@@ -1,6 +1,11 @@
 from perceptron import Perceptron
 
 class MultiplePerceptron(Perceptron):
+    def xor_gate(self, x, y):
+        return self.and_gate(self.or_gate(x, y), self.nand_gate(x, y))
 
-    def xor_gate(self):
-        and = self.function(theta, w1, w2)
+if __name__ == '__main__':
+    x = float(input("x => "))
+    y = float(input("y => "))
+    multiple_perceptron = MultiplePerceptron()
+    print(multiple_perceptron.xor_gate(x, y))
--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,43 +1,40 @@
 class Perceptron:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def function(self, theta, w1, w2):
-        tmp = self.x * w1 + self.y * w2
+    def function(self, theta, w1, w2, x, y):
+        tmp = x * w1 + y * w2
         if tmp > theta:
             return 1
         return 0
 
-    def and_gate(self):
+    def and_gate(self, x, y):
         w1 = 0.5
         w2 = 0.5
         theta = 0.7
-        return self.function(theta, w1, w2)
+        return self.function(theta, w1, w2, x, y)
 
-    def or_gate(self):
+    def or_gate(self, x, y):
         w1 = 0.7
         w2 = 0.7
         theta = 0.3
-        return self.function(theta, w1, w2)
+        return self.function(theta, w1, w2, x, y)
 
-    def nand_gate(self):
+    def nand_gate(self, x, y):
         w1 = -0.3
         w2 = -0.3
         theta = -0.5
-        return self.function(theta, w1, w2)
+        return self.function(theta, w1, w2, x, y)
 
-    def nor_gate(self):
+    def nor_gate(self, x, y):
         w1 = -0.5
         w2 = -0.5
         theta = -0.3
-        return self.function(theta, w1, w2)
+        return self.function(theta, w1, w2, x, y)
 
 
-x = float(input("x => "))
-y = float(input("y => "))
-perceptron = Perceptron(x, y)
-print("AND: " + str(perceptron.and_gate()))
-print("OR: " + str(perceptron.or_gate()))
-print("NAND: " + str(perceptron.nand_gate()))
-print("NOR: " + str(perceptron.nor_gate()))
+if __name__ == '__main__':
+    x = float(input("x => "))
+    y = float(input("y => "))
+    perceptron = Perceptron()
+    print("AND: " + str(perceptron.and_gate(x, y)))
+    print("OR: " + str(perceptron.or_gate(x, y)))
+    print("NAND: " + str(perceptron.nand_gate(x, y)))
+    print("NOR: " + str(perceptron.nor_gate(x, y)))
